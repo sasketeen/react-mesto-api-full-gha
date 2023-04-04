0b4b3cd5b8d3ebab66b231a9bac5d976e34f2aa5
Stas Proydakov <stasprojdakov@icloud.com>
2023-04-04 19:28:47 +0300

    feat: вернул роут для краштеста

--- backend/app.js
+++ backend/app.js
@@ -25,11 +25,11 @@ app.use(cors());
 
 app.use(requestLogger);
 
-// app.get('/crash-test', () => {
-//   setTimeout(() => {
-//     throw new Error('Сервер сейчас упадёт');
-//   }, 0);
-// });
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use('/', authRouter);
 app.use(auth);
